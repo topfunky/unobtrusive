a6000c22f06698e219fabdd020bc456c9cad5e7e
dwg <arni.dwg@gmail.com>
2008-12-22 01:05:10 +0000

    bugfix

--- generators/unobtrusive/unobtrusive_generator.rb
+++ generators/unobtrusive/unobtrusive_generator.rb
@@ -2,8 +2,7 @@ class UnobtrusiveGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      javascripts_dir = ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR
-
+      javascripts_dir = ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR.sub("#{Rails.root}#{File::SEPARATOR}", '')
       # Copy files
       Dir[File.dirname(__FILE__) + "/templates/*.js"].each do |template_filename_with_path|
         template_filename = File.basename(template_filename_with_path)
@@ -11,8 +10,8 @@ class UnobtrusiveGenerator < Rails::Generator::Base
       end
       FileUtils.mkdir_p(File.join(javascripts_dir, 'behaviours'))
       Dir[File.dirname(__FILE__) + "/templates/behaviours/*.js"].each do |template_filename_with_path|
-        template_filename = File.basename(template_filename_with_path)
-        m.file template_filename, File.join(javascripts_dir, 'behaviours', template_filename)
+        template_filename = File.join('behaviours', File.basename(template_filename_with_path))
+        m.file template_filename, File.join(javascripts_dir, template_filename)
       end
     end
   end
