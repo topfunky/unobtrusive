5b77bf4d961d27f33d2b7e884246b34ffe256638
dwg <arni.dwg@gmail.com>
2008-12-28 23:04:48 +0000

    expansion includes all behaviours before unobtrusive.js

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion(
-  :unobtrusive=>%w(lowpro behaviours/date_selector behaviours/draggable behaviours/inplace_editor unobtrusive)
+  :unobtrusive=>['lowpro'].concat(Dir.glob(File.join(ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR, 'behaviours', '*.js')).map {|fp| "behaviours/#{File.basename(fp).sub('.js', '')}"}) << 'unobtrusive'
 )
